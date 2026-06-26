a37a68364b40a306b5946b2325c56a097712dea8
sahil-dev28 <sahilkolge28@gmail.com>
2026-06-26 18:58:43 +0530

    refactor(controller): update productController logic
    * Improved structure and handling in productController

--- apps/server/src/controller/productController.ts
+++ apps/server/src/controller/productController.ts
@@ -1,13 +1,25 @@
 import type { Request, Response } from "express";
 
-import { Product } from "../models/Product";
+import { Category } from "@/models/Category";
+import { Product } from "@/models/Product";
 
 export const createProduct = async (
   req: Request,
   res: Response,
 ): Promise<void> => {
   try {
-    const newProduct = new Product(req.body);
+    const { category, ...rest } = req.body;
+
+    const categoryDoc = await Category.findOne({ name: category });
+
+    if (!categoryDoc) {
+      res.status(400).json({ message: "Invalid category" });
+    }
+
+    const newProduct = new Product({
+      ...rest,
+      categoryId: categoryDoc?._id,
+    });
 
     await newProduct.save();
 
